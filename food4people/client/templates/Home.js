8ceadf9155f1fc6c7b6a772f8fadc689bc16357e
Sanat Shah <sunnyshah711@gmail.com>
2015-10-17 06:09:30 -0400

    the bidness

--- food4people/client/templates/Home.js
+++ food4people/client/templates/Home.js
@@ -4,6 +4,9 @@ Template.Home.events({
 "click .signin": function () {
 	Router.go('Signin');
     },
+"click .signin2": function () {
+	Router.go('Signin');
+    },
 
 "click .signup": function () {
 	Router.go('Signup');
